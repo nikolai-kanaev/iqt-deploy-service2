268e2a4ab9a2b41f9f216177378335ee05e7d870
unknown <nikkan@SELNIKKAN.sweden.ecsoft>
2013-10-25 10:28:07 +0200

    Released version v4.2.6

--- src/SharedAssemblyInfo.cs
+++ src/SharedAssemblyInfo.cs
@@ -3,15 +3,15 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Security;
 [assembly: AssemblyTitle("IQT Deploy Service 2")]
-[assembly: AssemblyDescription("Deploying the IQT Service 0fe61a648546a40962cc513f550d76e45712ee0a - 2013-10-24 172447")]
+[assembly: AssemblyDescription("Deploying the IQT Service 0a97279627e91801836c291ea034be6b1dff4046 - 2013-10-25 100849")]
 [assembly: AssemblyCompany("Ciber")]
 [assembly: AssemblyProduct("IQT Deploy Service 2")]
 [assembly: AssemblyCopyright("Ciber")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("4.2.0.29809")]
-[assembly: AssemblyFileVersion("4.2.0.29809")]
+[assembly: AssemblyVersion("4.2.5.29810")]
+[assembly: AssemblyFileVersion("4.2.5.29810")]
 
-[assembly: AssemblyInformationalVersion("4.2.0.29809")]
+[assembly: AssemblyInformationalVersion("4.2.5.29810")]
 [assembly: CLSCompliantAttribute(false)]
 [assembly: AssemblyConfiguration("Release")]
 [assembly: Guid("e2a4af6b-1965-4ed6-af35-d79dda44467e")]
