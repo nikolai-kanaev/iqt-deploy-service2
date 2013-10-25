0a97279627e91801836c291ea034be6b1dff4046
unknown <nikkan@SELNIKKAN.sweden.ecsoft>
2013-10-25 10:08:49 +0200

    Released version v4.2.5

--- Rakefile.rb
+++ Rakefile.rb
@@ -185,4 +185,4 @@ end
 task :release => ["env:release", :msbuild, :output, :gittask] # JOBBA VIDARE HÄR
 task :output => [:peds_output]
 task :deploy_it => [:peds_zip, :copy_artifact, :map_network_drive, :find_version_number, :zip_copy_backup, :copy_artifact_for_deployment, :deploy_content, :remove_temp_artifact, :disconnect_network_drive]
-task :default  => ["env:release", "env:dev", "assemblyinfo", "msbuild", "output", "deploy_it", "publish"]
+task :default  => ["env:release", :assemblyinfo, :msbuild, :output, :deploy_it, :publish]
